fc3eda589ec1c014fc3bfe26bf4ded4bfaafd441
LAURYL <lorine@lamoh.LAMOH>
2026-02-01 12:28:14 +0300

    Connected new Habit to Track grid

--- js/index.js
+++ js/index.js
@@ -1,47 +1,10 @@
-
-//CALENDER CREATION FUNCTION
-
-function renderCalendar(container, date) {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const firstday = new Date(year, month, 1);
-    const lastday = new Date(year, month + 1, 0);
-    const daysInMonth = lastday.getDate();
-    const firstDayIndex = firstday.getDay();
-
-    console.log(firstDayIndex)
-
-
-//  monthYear.textContainer =
-
-    container.innerHTML = '';
-
-//  Spacers for the start of the month
-//  Pushes the 1st day of the month to the correct day column
-    for (let j = 0; j < firstDayIndex; j++) {
-        const spacer = document.createElement('div');
-        spacer.classList.add('habit-day');
-        container.appendChild(spacer);
-    }
-
-//  Current Month's Dates
-    for (let i = 1; i <= daysInMonth; i++) {
-        const dayDiv = document.createElement('div');
-
-        dayDiv.classList.add('habit-day');
-        dayDiv.textContent = i;
-
-        if (i === today.getDate() && month === today.getMonth() && year === today.getFullYear()) {
-            dayDiv.classList.add('today');
-        }
-        container.appendChild(dayDiv);
-    }
-    
-};
+const today = new Date();
+const year = today.getFullYear();
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
+    const date = new Date();
     const today = new Date();
     console.log(today)
 //  const daysContainer = document.getElementById('days');
@@ -56,12 +19,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const firstDayIndex = firstday.getDay();
 
 
+
+
     const containers = document.querySelectorAll('.habit-calender');
 
-    // containers.forEach(container => {
-    //     renderCalendar(container, today);
-    // })
-    containers.forEach(renderCalendar)
+    containers.forEach(container => {
+         renderCalendar(container, today);
+    })
+    // containers.forEach(renderCalendar)
 
 
     const months = [
@@ -70,21 +35,68 @@ document.addEventListener('DOMContentLoaded', function () {
     ];
 
     let currentDate = new Date();
-   
+
+
+    
+    
+    //CALENDER CREATION 
+
+    function renderCalendar(container, date) {
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const firstday = new Date(year, month, 1);
+        const lastday = new Date(year, month + 1, 0);
+        const daysInMonth = lastday.getDate();
+        const firstDayIndex = firstday.getDay();
+
+        console.log(firstDayIndex)
+
+
+    //  monthYear.textContainer =
+
+        container.innerHTML = '';
+
+    //  Spacers for the start of the month
+    //  Pushes the 1st day of the month to the correct day column
+        for (let j = 0; j < firstDayIndex; j++) {
+            const spacer = document.createElement('div');
+            spacer.classList.add('habit-day');
+            container.appendChild(spacer);
+        }
+
+    //  Current Month's Dates
+        for (let i = 1; i <= daysInMonth; i++) {
+            const dayDiv = document.createElement('div');
+            dayDiv.dataset.index = i;
+
+            dayDiv.classList.add('habit-day');
+            dayDiv.textContent = i;
+
+            if (i === today.getDate() && month === today.getMonth() && year === today.getFullYear()) {
+                dayDiv.classList.add('today');
+            }
+            container.appendChild(dayDiv);
+        }
+        
+    };
+
+
+    window.renderCalendar = renderCalendar;
+
 
 
     // Habit Growth Graph
 
     const grid = document.getElementById('track-grid');
 
-   const heatmapContainers = document.querySelectorAll('.track-grid');
+    const heatmapContainers = document.querySelectorAll('.track-grid');
 
     heatmapContainers.forEach(heatMapContainer => {
         renderGrid(heatMapContainer, today);
     })
 
 
-    function renderGrid(heatMapContainer) {
+    function renderGrid(heatMapContainer, today) {
         const year = today.getFullYear();
 
         const startOfYear = new Date(year, 0, 1); // jan 1st current year
@@ -94,19 +106,22 @@ document.addEventListener('DOMContentLoaded', function () {
         const dayOfYear = Math.floor(diffInMs/(1000 * 60 * 60 * 24)) + 1;
 
         // mock day completed data
-        const completedDays = [1, 2, 15, dayOfYear];
+        const completedDays = [1, 2, 15];
 
-        for(let i = 1; i < dayOfYear; i++){
+        for(let i = 1; i <= dayOfYear; i++){
             const day = document.createElement('div');
             day.classList.add('day');
         
-            if(completedDays.includes(i)){
-                day.classList.add('day-completed')
-            }
+            // if(completedDays.includes(i)){
+            //     day.classList.add('day-completed')
+            // }
 
             heatMapContainer.appendChild(day)
         }
     }
+
+    window.renderGrid = renderGrid;
+
 
 }
 );
@@ -128,7 +143,7 @@ const form = document.getElementById('habitForm');
 
 //default start date today
 const dateToday = new Date().toISOString().split("T")[0]; 
-startDateInput.value = dateToday;
+startDateInput.value = dateToday;                         
 
 openBtn.addEventListener("click", () => {
     modal.style.display = "flex";
@@ -166,10 +181,12 @@ form.addEventListener("submit", (e) => {
 // Add habit to UI
 
 const habitsGrid = document.getElementById("habit_grid")
+const trackhabitsGrid = document.getElementById("trackhabit_grid")
+
 
 function addHabitToUi(habit) {
     const habitCard = document.createElement('div');
-    habitCard.className = "habit-calender";
+    habitCard.className = "habit-card";
 
     habitCard.innerHTML = `
     <h4>${habit.habitname}</h4>
@@ -183,7 +200,58 @@ function addHabitToUi(habit) {
 
     habitsGrid.appendChild(habitCard);
 
+
+
+    // Habit Tracking
+    const habitTrackCard = document.createElement('div');
+    habitTrackCard.className = 'habit-card';
+
+    habitTrackCard.innerHTML = `
+        <h4>${habit.habitname}</h4>
+        <button>Mark as Complete</button>
+        <div class="habit-graph-wrapper">
+            <div class="day-labels">
+                <span>Mon</span>
+                <span>Tue</span>
+                <span>Wed</span>
+                <span>Thu</span>
+                <span>Fri</span>
+                <span>Sat</span>
+                <span>Sun</span>
+            </div>
+            <div id="track-grid" class="track-grid"></div>
+    `
+
+    trackhabitsGrid.appendChild(habitTrackCard);
+
+    const newGrid = habitTrackCard.querySelector('.track-grid');
+    renderGrid(newGrid, new Date(dateToday))
+
     //Render
     const newCalender = habitCard.querySelector(".habit-calender");
-    renderCalendar(newCalender) ;
+    renderCalendar(newCalender, new Date(dateToday)) ;
 }
+
+
+// Mark day complete
+
+const markCompleteBtn = document.getElementById('markAsComplete')
+
+markCompleteBtn.addEventListener("click", () => {
+
+    // difference in days(including today)
+    const startOfYear = new Date(year, 0, 1); // jan 1st current year
+
+    const diffInMs = today - startOfYear;
+
+    const index_today = Math.floor(diffInMs/(1000 * 60 * 60 * 24)) + 1;
+    console.log(index_today)
+
+    const completedDay = document.querySelector('.day')
+
+    console.log(completedDay[index_today])
+
+
+    completedDay[index_today].classList.add('day-completed')
+
+});
